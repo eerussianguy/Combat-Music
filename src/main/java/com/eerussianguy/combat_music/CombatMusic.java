9d5cac33142850875be84bdf34e62bf4d7c8c0dc
EERussianguy <eerussianguy@gmail.com>
2022-02-11 15:30:39 -0500

    1.18 update

--- src/main/java/com/eerussianguy/combat_music/CombatMusic.java
+++ src/main/java/com/eerussianguy/combat_music/CombatMusic.java
@@ -3,26 +3,26 @@ package com.eerussianguy.combat_music;
 import java.util.List;
 import java.util.Random;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.audio.SimpleSound;
-import net.minecraft.client.audio.SoundHandler;
-import net.minecraft.client.entity.player.ClientPlayerEntity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.monster.MonsterEntity;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.Difficulty;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.phys.AABB;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
+import net.minecraftforge.network.NetworkConstants;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import static com.eerussianguy.combat_music.CombatMusic.MOD_ID;
@@ -31,14 +31,15 @@ import static com.eerussianguy.combat_music.CombatMusic.MOD_ID;
 public class CombatMusic
 {
     public static final String MOD_ID = "combat_music";
+
     private static final Logger LOGGER = LogManager.getLogger();
 
     public int decaySeconds = 0;
-    public SimpleSound lastSound;
+    public SimpleSoundInstance lastSound;
 
     public CombatMusic()
     {
-        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         MinecraftForge.EVENT_BUS.addListener(this::onAttack);
         MinecraftForge.EVENT_BUS.addListener(this::onClientTick);
 
@@ -50,10 +51,10 @@ public class CombatMusic
         Minecraft mc = Minecraft.getInstance();
         if (event.phase == TickEvent.Phase.END && mc.level != null && mc.level.getGameTime() % 20 == 0)
         {
-            SoundHandler manager = mc.getSoundManager();
+            SoundManager manager = mc.getSoundManager();
             if (manager.isActive(lastSound))
             {
-                ClientPlayerEntity player = mc.player;
+                LocalPlayer player = mc.player;
                 if (player != null && getEntities(player) == 0)
                 {
                     decaySeconds++;
@@ -69,11 +70,10 @@ public class CombatMusic
     private void onAttack(final LivingAttackEvent event)
     {
         LivingEntity entity = event.getEntityLiving();
-        if (entity instanceof ClientPlayerEntity)
+        if (entity instanceof LocalPlayer player)
         {
-            ClientPlayerEntity player = (ClientPlayerEntity) entity;
             Minecraft mc = Minecraft.getInstance();
-            SoundHandler manager = mc.getSoundManager();
+            SoundManager manager = mc.getSoundManager();
             if (mc.options.difficulty != Difficulty.PEACEFUL)
             {
                 if (getEntities(player) > CMConfig.CLIENT.minPursuitEntities.get())
@@ -90,12 +90,12 @@ public class CombatMusic
         }
     }
 
-    private static int getEntities(ClientPlayerEntity player)
+    private static int getEntities(LocalPlayer player)
     {
-        return player.clientLevel.getEntitiesOfClass(MonsterEntity.class, new AxisAlignedBB(-12D, -10D, -12D, 12D, 10D, 12D).move(player.blockPosition()), mob -> mob.canSee(player)).size();
+        return player.clientLevel.getEntitiesOfClass(Monster.class, new AABB(-12D, -10D, -12D, 12D, 10D, 12D).move(player.blockPosition()), mob -> mob.canAttack(player, TargetingConditions.DEFAULT)).size();
     }
 
-    private static SimpleSound pickSound(Random rand)
+    private static SimpleSoundInstance pickSound(Random rand)
     {
         final List<? extends String> sounds = CMConfig.CLIENT.sounds.get();
         final int idx = rand.nextInt(sounds.size());
@@ -104,6 +104,6 @@ public class CombatMusic
         {
             throw new NullPointerException("Invalid sound event resource location detected: " + sounds.get(idx));
         }
-        return SimpleSound.forMusic(sound);
+        return SimpleSoundInstance.forMusic(sound);
     }
 }
